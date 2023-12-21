6551986893f320578934fb5f7547b3af7b33eb26
knlao <1166286-7@g.puiching.edu.mo>
2023-12-21 21:26:29 +0800

    added credits

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,5 +1,13 @@
 <template>
-    <p>About</p>
+    <p class="header">About</p>
     <p>knlao.github.io</p>
     <p>find me in github</p>
+    <p class="header">Credits</p>
+    <p><a href="https://github.com/rafgraph/spa-github-pages">https://github.com/rafgraph/spa-github-pages</a> for the index.html and 404.html script</p>
 </template>
+
+<style scoped>
+.header {
+    font-weight: bold;
+}
+</style>
