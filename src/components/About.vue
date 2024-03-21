0178037624e18bf3b75e727c204159c5f1916cb9
knlao <1166286-7@g.puiching.edu.mo>
2024-03-21 21:06:25 +0800

    Update About.vue

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,7 @@
 <template>
     <p class="header">About</p>
     <p>knlao.github.io</p>
+    <p>I like to make useless programs</p>
     <p>find me in github</p>
     <p class="header">Credits</p>
     <p><a href="https://github.com/rafgraph/spa-github-pages">https://github.com/rafgraph/spa-github-pages</a> for the index.html and 404.html script</p>
